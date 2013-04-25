5e055ef1dbf4f8484008cb95d44e3ceb29674bd2
cloubrain <cloubrain@cloubrain-virtual-machine.(none)>
2013-04-25 15:36:15 -0700

    updated for multi-user and large DB

--- README.md
+++ README.md
@@ -7,9 +7,9 @@ Just type:
 Scripts to pull cloud data from mongoDB server.
 
 rabbitmongo.py
-Moves data from RabbitMQ queue to mongoDB
+Moves data from RabbitMQ queue to mongoDB on large EBS volume
 
 cbmongo.py
-Script to pull data from mongoDB on data collection server
+Script to pull data from mongoDB
 Stores data to a dict called: vms
 print vms
--- cbmongo.py
+++ cbmongo.py
@@ -2,12 +2,14 @@
 # cbmongo.py
 # This tests the MongoDB interface
 # Data is pulled from mongoDB and put into a dict: vms
+#  mongoDB is on EBS volume (currently 100GB)
 #
 # TYPE: python cbmongo.py
 #
 # Here is the code that writes to the server:
-#  https://github.com/cloubrain/datacollector/tree/master/vmware
-#
+#  https://github.com/cloubrain/vmwaredata
+# Multi-user version
+# 
 # ssh into the server:
 #  ssh -i cbopen.pem ec2-user@myhost
 # make sure to: chmod 400 *.pem
@@ -24,14 +26,16 @@ import sys
 import time
 import pymongo
 
-# This host is the data collection host
-myhost = "ec2-54-234-14-163.compute-1.amazonaws.com" # Live data streaming to this server
+# This host has about a day of data in mongoDB
+myhost = "ec2-54-234-14-163.compute-1.amazonaws.com"
 print "***************"
 print "myhost = " + myhost
 print "***************"
 
 dbname = "vmware"
 statscollname = "stats_new"
+DCname = "dkan-cluster-1-dc-19"			#DC name for which you want to extract
+
 dbcon = pymongo.Connection(myhost)		       	# opens connection to the DB
 dbpointer = dbcon[dbname]			      	# selects the DB as dbname
 dbstatscoll = dbpointer[statscollname]			# selecting the relevant collection
@@ -46,30 +50,28 @@ dbstatscoll = dbpointer[statscollname]			# selecting the relevant collection
 #print "***************"
 print "*** Store DB to vms dict: Takes time to transfer ***"
 #print "*** DB stats: ~2s/MB ***"
-#statresp = dbpointer.command({'dbstats': 1})
-#print statresp
+statresp = dbpointer.command({'dbstats': 1})
+print statresp
 
 # The DB has a lot of info about many Virtual Machines (VMs)
 vmdb = dbpointer["vm"]
-
-vms = {}			       			#data structure for VM data
-for vm in vmdb.find():
+vms = {}	
+print "List of VMs in DC (" + DCname + "):"		       			#main data structure
+for vm in vmdb.find({"DCname" : DCname}):
 	vms[vm["name"].strip()] = []			#storing all VMs ID into this dict
 
 numvms = len(vms)
 print "*** # VMs in DB:", numvms, "***"
 
-perfkeysdb = dbpointer["perf_counter_id_map"]
+perfkeysdb = dbpointer["perfCounterIdMap"]
 perfkeys = {}
-for pk in perfkeysdb.find():
-	perfkeys[pk["key"]] = pk["value"]	       	#storing (mapping) all performance metrics' keys with their name here
+for pk in perfkeysdb.find({"DCname": DCname}):
+	perfkeys[pk["k"]] = pk["v"]	       	#storing (mapping) all performance metrics' keys with their name here
 
-numpk = len(perfkeys)
-print "*** # perf keys:", numpk, "***"
 print "*** Extracting Data from DB ***"
 cnt = 0
 print "* Progress..."
-for c in dbstatscoll.find():
+for c in dbstatscoll.find({"DCname":DCname}):
 	cnt += 1
 	if cnt%100000 is 0:
 		print cnt/100000  # Progress indicator
@@ -77,7 +79,7 @@ for c in dbstatscoll.find():
 	r = c
 	try:
 		r["perfkey"] = perfkeys[c["key"]]
-		vms[c["name"]].append(r)		#storing the stats for vm in "vms" dict
+		vms[c["name"]].append(r)			#storing the stats for vm in "vms" dict
 	except:
 		pass
 	
@@ -90,16 +92,12 @@ for vm in vms:
 	vmcount += 1    # vmcount increment for each VM
 	stcount = 0    # timestamp counter
 	t = []
+	#tm = time.time()
 	for st in vms[vm]:
-		stcount += 1
 		t.append(st["perfkey"] + ":" + str(st["value"]))
 		tm = st["time_current"]
-		t.append(":Time:" + str(tm))
-	print "******"
-	print vm, "::", ":".join(t)
-	#print vm
-	print "*** data history count:", stcount
-	#print "*" , "::", vm, ":", ":".join(t)
+	#print vm, "::", ":".join(t)
+	print "DBtime: ", tm , "::", vm, ":", ":".join(t)
 
 print "***************"
 print "VMcount: " , vmcount
@@ -107,3 +105,4 @@ print "VMcount: " , vmcount
 print "***************"
 print "Done!"
 print "***************"
+
